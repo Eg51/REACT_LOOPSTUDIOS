c972c92eb22df49a87a783f272f8425835520f51
Your Name <dciphererz@gmail.com>
2026-05-25 19:13:12 +0100

    vmodified:   app/favicon.ico #    modified:   app/page.tsx #    deleted:    public/images/favicon-32x32.png

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="
+    ">
+      <img src="" alt="" />
     </div>
   );
 }
