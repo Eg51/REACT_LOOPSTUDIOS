06ced572777192b4a989ba58958aa3a2f0080036
Your Name <dciphererz@gmail.com>
2026-06-01 16:10:30 +0100

    used the next js attribute of image. i also injected Sir franklin code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { Fullscreen } from "@boxicons/react";
 import Image from "next/image";
 import Link from 'next/link'
+import {ShoppingBag, User, Heart, ClipboardList, MessageCircle, Settings, ChevronRight,} from "lucide-react"; // injected code start
+
+
 
 export default function Home() {
   return (
-    <div className="flex items-ccenter justify-center">
+    <>
+    <div className="flex items-center justify-center">
       <div className="flex flex-col h-auto w-full">
         <img src="/images/mobile/image-hero.jpg" className="bg-auto pb-18"/>
-
         <div className="absolute flex p-11 justify-between
           w-full max-w-['375px']"
           >
@@ -22,6 +25,7 @@ export default function Home() {
             alt="logo"
             width={30}
             height={60}
+            className="cursor-pointer md:hidden"
             />
         </div>
         <img src="/images/mobile/image-interactive.jpg" alt="interactive_image"
@@ -105,5 +109,85 @@ export default function Home() {
         </div>
       </div>
     </div>
+
+  <div className="p-4">
+      <Link
+        href="/"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          {/* <Home size={18} /> */}
+          <span className="font-medium">Home</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/shop"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <ShoppingBag size={22} />
+          <span className="font-medium">Shop</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/collections"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <Heart size={22} />
+          <span className="font-medium">Collections</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/account"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <User size={22} />
+          <span className="font-medium">Account</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/orders"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <ClipboardList size={22} />
+          <span className="font-medium">Orders</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/support"
+        className="mb-2 flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <MessageCircle size={22} />
+          <span className="font-medium">Support</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+
+      <Link
+        href="/settings"
+        className="flex items-center justify-between rounded-2xl px-4 py-4 hover:bg-gray-50"
+      >
+        <div className="flex items-center gap-4">
+          <Settings size={22} />
+          <span className="font-medium">Settings</span>
+        </div>
+        <ChevronRight size={18} className="text-gray-400" />
+      </Link>
+  </div>
+    </>
   );
 }
